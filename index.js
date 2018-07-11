d548b8952b794382f6037ad4845e42a47f535a65
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-11 21:47:10 +0530

    vid 27 installed nodemon

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
     }
   )
 );
@@ -21,6 +23,8 @@ app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
