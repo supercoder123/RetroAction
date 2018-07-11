8f6ed2e337b7c752ffffdd0f764f0794a08e7591
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-11 17:14:00 +0530

    vid 23 oauth added scope

--- index.js
+++ index.js
@@ -17,5 +17,10 @@ passport.use(
   )
 );
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
